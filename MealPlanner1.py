3c7058ddb7ba42f29444bfdc83ee8517a9458cb1
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-11-25 21:09:20 +0000

    test1

--- MealPlanner1.py
+++ MealPlanner1.py
@@ -1,1 +1,39 @@
-print("hello")
+
+class user():
+    userName = input("whats your name?")
+    calories = float(input("Whats your calories requirements?"))
+    protein = float(input("Whats your protein requirements?"))
+    carbs = float(input("Whats your carbs requirements?"))
+    fat = float(input("Whats your fat requirements?"))
+
+
+class chicken():
+    calories = 10
+    fat = 10
+    carbs = 10
+    protein = 10
+
+class salad():
+    calories = 10
+    fat = 10
+    carbs = 10
+    protein = 10
+
+class potato():
+    calories = 10
+    fat = 10
+    carbs = 10
+    protein = 10
+
+class fish():
+    calories = 10
+    fat = 10
+    protein = 10
+    carbs = 10
+
+user()
+
+if user.calories == chicken.calories :
+    print("test success")
+else :
+    print("test failed")
