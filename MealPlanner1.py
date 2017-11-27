c9e3cd77ecd08510cf7101856817657bd382700e
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-11-27 20:52:35 +0000

    test2

--- MealPlanner1.py
+++ MealPlanner1.py
@@ -1,6 +1,5 @@
 
 class user():
-    userName = input("whats your name?")
     calories = float(input("Whats your calories requirements?"))
     protein = float(input("Whats your protein requirements?"))
     carbs = float(input("Whats your carbs requirements?"))
@@ -9,7 +8,7 @@ class user():
 
 class chicken():
     grams = 100
-    calories = 31
+    calories = 150
     fat = 10
     carbs = 10
     protein = 10
@@ -24,10 +23,20 @@ class salad():
 user()
 
 # calculates how much grams of chicken you need to match your requirements of calories for it to 3 decimal places
-def calc():
-        if user.calories == chicken.calories:
-            print("test success")
-        else:
-            holder = user.calories / chicken.calories
-            amount = holder * chicken.grams
-            print("%.3f" % amount, " grams of chicken for ", user.calories, " required calories")
+
+if user.calories == chicken.calories :
+    print("test success")
+else :
+    holder = user.calories / chicken.calories
+    amount = holder * chicken.grams
+    print("%.3f" % amount, " grams of chicken for ", user.calories, " required calories")
+
+holderone = user.calories / salad.calories
+holdertwo = user.calories / chicken.calories
+holderone = holderone / 2
+holdertwo = holdertwo / 2
+amountone = holderone * salad.grams
+amounttwo = holdertwo * chicken.grams
+print("%.3f" % amountone, " - this is the amount of grams for salad")
+print("%.3f" % amounttwo, " - this is the amount of grams for chicken")
+# this works out how much calories is needed for two foods.
