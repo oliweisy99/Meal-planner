158cb69a0e6ec98e89e32a75149300069f9aad96
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-04 18:25:12 +0000

    test4

--- MealPlanner1.py
+++ MealPlanner1.py
@@ -22,21 +22,26 @@ class salad():
 
 user()
 
-# calculates how much grams of chicken you need to match your requirements of calories for it to 3 decimal places
+saladR = salad.calories / salad.protein
+chickR = chicken.calories / chicken.protein
+Co = user.protein - 1
+Ct = 1
 
-if user.calories == chicken.calories :
-    print("test success")
-else :
-    holder = user.calories / chicken.calories
-    amount = holder * chicken.grams
-    print("%.3f" % amount, " grams of chicken for ", user.calories, " required calories")
 
-holderone = user.calories / salad.calories
-holdertwo = user.calories / chicken.calories
-holderone = holderone / 2
-holdertwo = holdertwo / 2
-amountone = holderone * salad.grams
-amounttwo = holdertwo * chicken.grams
-print("%.3f" % amountone, " - this is the amount of grams for salad")
-print("%.3f" % amounttwo, " - this is the amount of grams for chicken")
-# this works out how much calories is needed for two foods.
+while True :
+    x = Co * saladR
+    y = Ct * chickR
+    total = x + y
+    if total == user.calories :
+        Sfo = Co / salad.protein
+        Sft = Ct / chicken.protein
+        A = Sfo * salad.grams
+        B = Sft * chicken.grams
+        print("you need ", "%.2f" % A, " grams of salad to meet your needs ")
+        print("you need ", "%.2f" % B, " grams of Chicken to meet your needs ")
+        break
+    else :
+        Co -= 1
+        Ct += 1
+
+print("check")
