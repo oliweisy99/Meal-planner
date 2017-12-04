229eae5647a8a59413956b0828d2d4bc340e3c4d
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-04 18:26:19 +0000

    test4

--- oldcode.py
+++ oldcode.py
@@ -1,19 +1,19 @@
 
 # calculates how much grams of chicken you need to match your requirements of calories for it to 3 decimal places
 
-if user.calories == chicken.calories :
-    print("test success")
-else :
-    holder = user.calories / chicken.calories
-    amount = holder * chicken.grams
-    print("%.3f" % amount, " grams of chicken for ", user.calories, " required calories")
-
-holderone = user.calories / salad.calories
-holdertwo = user.calories / chicken.calories
-holderone = holderone / 2
-holdertwo = holdertwo / 2
-amountone = holderone * salad.grams
-amounttwo = holdertwo * chicken.grams
-print("%.3f" % amountone, " - this is the amount of grams for salad")
-print("%.3f" % amounttwo, " - this is the amount of grams for chicken")
+#if user.calories == chicken.calories :
+#    print("test success")
+#else :
+ #   holder = user.calories / chicken.calories
+ #   amount = holder * chicken.grams
+ #   print("%.3f" % amount, " grams of chicken for ", user.calories, " required calories")
+#
+#holderone = user.calories / salad.calories
+#holdertwo = user.calories / chicken.calories
+#holderone = holderone / 2
+#holdertwo = holdertwo / 2
+#amountone = holderone * salad.grams
+#amounttwo = holdertwo * chicken.grams
+#print("%.3f" % amountone, " - this is the amount of grams for salad")
+#print("%.3f" % amounttwo, " - this is the amount of grams for chicken")
 # this works out how much calories is needed for two foods.
