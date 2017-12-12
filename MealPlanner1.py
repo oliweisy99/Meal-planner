4e2b36306d5be1a80e3415a68703a7b1a8cd387a
OliverWeisfeld <oliverweisfeld@gmail.com>
2017-12-12 17:42:57 +0000

    test5

--- MealPlanner1.py
+++ MealPlanner1.py
@@ -23,39 +23,19 @@ Co = user.protein
 # setting the counts to go through all the possibilities
 Ct = 0
 
-def repeat(inputo,inputt) :
-    A = (inputo / salad.protein) * salad.grams
-    B = (inputt / chicken.protein) * chicken.grams
-    print("you need ", "%.2f" % A, " grams of salad to meet your needs ")
-    print("you need ", "%.2f" % B, " grams of Chicken to meet your needs ")
 
-while True :
-    Co -= 1
-    Ct += 1
-    H = Co - 1 # this is the one after going through the list of possiblities
-    T = Ct + 1
-    x = Co * saladR # counts multiplied by ratios to find calorie amount
-    y = Ct * chickR
-    total = x + y
-    sum = ((H * saladR) + (T * chickR)) - user.calories
-    Tmu = total - user.calories
-    if total == user.calories : # if total is equal to the required calorie amount
-        repeat(Co, Ct)
-        break
-    else :
-        if Tmu > 0 :
-            if sum < Tmu :
-                continue
-            else :
-                repeat(H, T)
-                break
-        else :
-            temp = user.calories - total
-            if temp > sum :
-                continue
-            else :
-                repeat(H, T)
-                break
+uc = user.calories
+total = Co * saladR
+if total < uc :
+    need = uc - total
+else : 
+    need = total - uc
+stepV = chickR - saladR
+if stepV < 0 :
+    stepV = stepV * -1
+minA = need // stepV
+A = ((Co - minA) / salad.protein) * 100
+B = ((Ct + minA) /  chicken.protein) * 100
+print("you need ", "%.2f" % A, " grams of salad to meet your needs ")
+print("you need ", "%.2f" % B, " grams of Chicken to meet your needs ")
 
-# next stage is adding in more foods, do 5 foods to create one meal, then do that for carbs and fat
-# after that, start creating
