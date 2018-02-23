e29df1329a4b1311941ba55b2f2a2eaddc435e35
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-02-23 21:48:20 +0000

    test6

--- MealPlanner1.py
+++ MealPlanner1.py
@@ -53,8 +53,13 @@ class number:
         self.one = 0
         self.two = 0
         self.three = 0
+        self.difference = 0
 
 myList = [number() for index in range(0,5)]
+myList[0].one = 2
+myList[0].two = 4
+myList[0].three = 5
+
 
 
 one = 0.1
@@ -83,6 +88,16 @@ for z in range(0,20):
                     holder = orderArr[index]
                     orderArr[index] = totalDif
                     orderArr[index - 1] = holder
+
+            '''
+            if myList[0].difference > totaldif:
+                holder = ml.d[index]
+                ml.d[index] = totalDif
+                ml.d[index - 1] = holder
+                mylist[index].one = one
+                mylist[index].two = two
+                mylist[index].three = three
+            '''
             # sorting array thing
             three = three + 0.1
             if three > 2.1:
@@ -102,8 +117,11 @@ procedures, multi threading, 0.001, using less variables, making code more effic
 - make sure it works with multiple amounts of foods to create actual meals, so have lots of foods and 
 make 3 different meals based on the foods to get really close amounts. 
 
-once you have the code to generate the best variations, now you can start making meals best on requriements 
-then you can start adding features or start making it into a website 
-then you start adding more features and get the business side of it sorted
+start making good meals based on variations and categories etc so all is needed is just switching of foods into 
+different meals - sandwiches, salads, meat meals, cereals etc.
+once you have the code to generate the best variations, now you can start making meals best on requirements such as 
+price, needs, aims and start adding in foods from websites to get it done. 
+then you can start adding more features or start making it into a website 
+then you start getting the business side of it sorted so it can be shopped, made and delivered
 '''
 
