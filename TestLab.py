2f113009cffedcf836737f32a78b3981c66a0f78
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-05-23 21:39:04 +0100

    Work in progress

--- TestLab.py
+++ TestLab.py
@@ -1,21 +1,26 @@
 notes = open("Notes", "r+")
-notes.seek(70,0)
+n = 70
+notes.seek(n,0)
 check = notes.read(1)
 assign = ""
 bool = True
-while check != "]":
-    notes.seek(71,0)
-    assign = notes.read(4)
-    print(assign)
-    notes.seek(75,0)
+#count = 0
+
+
+if check == "[":
     while bool == True:
+        notes.seek(n+1)
+        assign = notes.read(4)
+        #foodList[index].tags[count] = notes.read(4)
+        n = n + 5
+        notes.seek(n)
         if notes.read(1) == ",":
-            notes.seek(76,0) # 76 is a variable that gets added on
-            assign = notes.read(4)
-            print(assign)
+            #count = count + 1
+            continue
         else:
-            check = "]"
             bool = False
+            break
+
 '''
         while fo.read(variable) != "]":
         if fo.read(i+1) where it is only reading one character == "[" and fo.read(whatever) != "]":
@@ -30,4 +35,5 @@ while check != "]":
         loop until fo.read(wtever) == "]"
         then finish 
 print(check)
+
 '''
