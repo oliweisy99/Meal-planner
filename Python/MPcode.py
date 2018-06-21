9aab02f42ed7e110f038c4fa9adce0d995b8095c
OliverWeisfeld <oliverweisfeld@gmail.com>
2018-06-21 22:25:48 +0100

    Work in progress

--- Python/MPcode.py
+++ Python/MPcode.py
@@ -2,3 +2,12 @@
 # Macro needs
 # Price range
 # then calculate BREK DINR LNCH & SNCK using Macros and price
+
+userCal = float(input("Whats your calories requirements?"))
+userPro = float(input("Whats your protein requirements?"))
+userCarb = float(input("Whats your carb requirements?"))
+userFat = float(input("Whats your fat requirements?"))
+priceRange = float(input("What is your price range?"))
+
+
+
